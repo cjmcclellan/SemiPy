8176496f60a189e1f076a5c1e73b5ba44722d84e
cjmcclellan <cmcclellan1993@gmail.com>
2020-08-11 00:02:40 -0700

    added pFET data to test_fetextractor

--- SemiPy/Extractors/Transistor/test_fetextractor.py
+++ SemiPy/Extractors/Transistor/test_fetextractor.py
@@ -5,7 +5,8 @@ import unittest
 from SemiPy.Extractors.Transistor.FETExtractor import FETExtractor
 from SemiPy.Devices.Materials.TwoDMaterials.TMD import MoS2
 from SemiPy.Devices.Materials.Oxides.MetalOxides import SiO2
-from SemiPy.Devices.Devices.FET.ThinFilmFET import nTFT
+from SemiPy.Devices.Materials.Semiconductors.BulkSemiconductors import Silicon
+from SemiPy.Devices.Devices.FET.ThinFilmFET import nTFT, pTFT
 from SemiPy.helper.paths import get_abs_semipy_path
 
 from physics.value import Value, ureg
@@ -17,30 +18,36 @@ class TestFETExtractors(unittest.TestCase):
 
     def test_fetextraction(self):
 
-        # plotly.io.orca.config.executable = '/node_modules/orca/bin/orca'
         idvd_path = get_abs_semipy_path('SampleData/FETExampleData/WSe2_Sample_4_Id_Vd.txt')
         idvg_path = get_abs_semipy_path('SampleData/FETExampleData/WSe2_Sample_4_Id_Vg.txt')
-        # path = '/home/connor/Documents/Stanford_Projects/Extractions/src/SampleData/FETExampleData/nano_patterning.csv'
-        # idvd_path = '/home/connor/Documents/Stanford_Projects/Extractions/SemiPy/SampleData/FETExampleData/WSe2_Sample_4_Id_Vd.txt'
-        # idvg_path = '/home/connor/Documents/Stanford_Projects/Extractions/SemiPy/SampleData/FETExampleData/WSe2_Sample_4_Id_Vg.txt'
-        #
-        #idvd_path = '/home/connor/Documents/Stanford_Projects/Extractions/fetextraction/SemiPy/SampleData/FETExampleData/WSe2_Sample_4_Id_Vd.txt'
-        #idvg_path = '/home/connor/Documents/Stanford_Projects/Extractions/fetextraction/SemiPy/SampleData/FETExampleData/WSe2_Sample_4_Id_Vg.txt'
-        idvd_path = '/home/connor/Documents/Stanford_Projects/Extractions/fetextraction/SemiPy/SampleData/FETExampleData/WSe2_Sample_4_Id_Vd.txt'
-        idvg_path = '/home/connor/Documents/Stanford_Projects/Extractions/fetextraction/SemiPy/SampleData/FETExampleData/WSe2_Sample_4_Id_Vg.txt'
 
         gate_oxide = SiO2(thickness=Value(30, ureg.nanometer))
         channel = MoS2(layer_number=1)
+        substrate = Silicon()
 
-        fet = nTFT(gate_oxide=gate_oxide, channel=channel, width=Value(1, ureg.micrometer), length=Value(1, ureg.micrometer))
-
-        # SchotkkyModel(fet, )
+        fet = nTFT(gate_oxide=gate_oxide, channel=channel, width=Value(1, ureg.micrometer), length=Value(1, ureg.micrometer),
+                   substrate=substrate)
 
         result = FETExtractor(FET=fet, idvg_path=idvg_path, idvd_path=idvd_path)
 
+        # nFET data
         self.assert_value_equals(result.FET.Vt_avg, Value(3.78, ureg.volt), 'Vt avg')
         self.assert_value_equals(result.FET.Vt_bwd, Value(4.06, ureg.volt), 'Vt bwd')
         self.assert_value_equals(result.FET.Vt_fwd, Value(3.5, ureg.volt), 'Vt fwd')
+        self.assert_value_equals(result.FET.min_ss, Value(745.9, ureg.meter * ureg.milivolt / ureg.ampere), 'min SS')
+        self.assert_value_equals(result.FET.max_gm, Value(3.64, ureg.microsiemes / ureg.micrometer), 'Max Gm')
+        
+        fet = pTFT(gate_oxide=gate_oxide, channel=channel, width=Value(1, ureg.micrometer), length=Value(1, ureg.micrometer),
+                   substrate=substrate)
+
+        result = FETExtractor(FET=fet, idvg_path=idvg_path, idvd_path=idvd_path)
+
+        # pFET data
+        self.assert_value_equals(result.FET.Vt_avg, Value(-15.88, ureg.volt), 'Vt avg')
+        self.assert_value_equals(result.FET.Vt_bwd, Value(-14.96, ureg.volt), 'Vt bwd')
+        self.assert_value_equals(result.FET.Vt_fwd, Value(-16.81, ureg.volt), 'Vt fwd')
+        self.assert_value_equals(result.FET.max_gm, Value(0.8, ureg.microsiemes / ureg.micrometer), 'Max Gm')
+        self.assert_value_equals(result.FET.min_ss, Value(482.5, ureg.meter * ureg.milivolt / ureg.ampere), 'min SS')
         #self.assert_value_equals(result.FET.max_mobility, Value(15.82, ureg.centimeter ** 2 / ureg.second / ureg.volt), 'Max Mobility')
 
         result.FET.publish_csv('.')
